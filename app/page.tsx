4a0c6e5b8a1252b7ed0ca011220324ec5aec5843
Yami <raphaelribeirocaetano@gmail.com>
2025-05-13 00:10:44 -0300

    add header, menu and shops

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,168 @@
 import Image from "next/image";
+import Header from "@/public/header.png";
+import Avatar from "@/public/avatar.png";
+import Dragon from "@/public/dragon.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="bg-linear-to-b from-[#0C2231] to-[transparent]">
+      <div className="container flex flex-col items-center justify-center min-h-screen mx-auto">
+      <div className="min-h-screen w-[1020px] py-2 ">
+        <header className="flex">
+          <Image src={Header.src} width={1020} height={208} alt="Header"></Image>
+        </header>
+        <div className="flex w-full mt-5 justify-center items-center">
+          <div className="flex space-between w-full gap-5">
+            <div className="border-transparent border-b-3 hover:border-white cursor-pointer">
+              <h3 className="text-white text-3xl">Lojas</h3>
+            </div>
+            <div className="border-b-3 border-transparent hover:border-white cursor-pointer opacity-50">
+              <h3 className="text-white text-3xl ">Negociações</h3>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex gap-5 items-center justify-center">
+            <div className="border-2 border-white/50 rounded-lg">
+            <input type="text" placeholder="Buscar por mobi ou usuário" className="bg-white/30 text-white font-bold rounded p-2 bg-[url('/lupa.png')] bg-no-repeat bg-left pl-5 w-60 outline-none"/>
+          </div>
+          <div className="flex items-center justify-center habbo-component rounded-lg p-2 bg-[#9E6D43] hover:bg-[#AF8349] cursor-pointer">
+            <div className="flex border-1 border-black rounded w-32 items-center justify-center"> 
+              <div className="flex">
+              <Image src={Avatar.src} width={28} height={28} alt="Avatar"></Image>
+            </div>
+            <p className="text-md text-white">Minha Loja</p>
+            </div>
+          </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <section className="flex flex-wrap gap-3 mt-5">
+          <div className="flex habbo-component rounded-lg bg-[#6794A7] w-60 h-full justify-center p-3">
+            <div className="flex-col p-3 bg-[#EFEFEF] rounded-lg border-1 border-black w-full h-full">
+              <div className="flex gap-3 mb-2">
+                <div>
+                  <Image src={Avatar.src} width={28} height={28} alt="Avatar"></Image>
+                </div>
+                <div>
+                  <p>Loja de <b>Yami</b></p>
+                </div>
+              </div>
+              <div className="flex flex-wrap border-1 border-black p-2 gap-3 h-full rounded">
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex habbo-component rounded-lg bg-[#6794A7] w-60 h-full justify-center p-3">
+            <div className="flex-col p-3 bg-[#EFEFEF] rounded-lg border-1 border-black w-full h-full">
+              <div className="flex gap-3 mb-2">
+                <div>
+                  <Image src={Avatar.src} width={28} height={28} alt="Avatar"></Image>
+                </div>
+                <div>
+                  <p>Loja de <b>Yami</b></p>
+                </div>
+              </div>
+              <div className="flex flex-wrap border-1 border-black p-2 gap-3 h-full rounded">
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex habbo-component rounded-lg bg-[#6794A7] w-60 h-full justify-center p-3">
+            <div className="flex-col p-3 bg-[#EFEFEF] rounded-lg border-1 border-black w-full h-full">
+              <div className="flex gap-3 mb-2">
+                <div>
+                  <Image src={Avatar.src} width={28} height={28} alt="Avatar"></Image>
+                </div>
+                <div>
+                  <p>Loja de <b>Yami</b></p>
+                </div>
+              </div>
+              <div className="flex flex-wrap border-1 border-black p-2 gap-3 h-full rounded">
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+                <div id="mobi" className="flex flex-col items-center justify-center w-12 bg-white border-1 border-black rounded">
+                  <Image src={Dragon.src} width={50} height={50} alt="Dragon"></Image>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </section>
+      </div>
     </div>
+    </main>
   );
 }
